565433f96c4229c806f8fb9e028d82515cb9e595
Marco Botto <marco.botto@gmail.com>
2016-06-04 01:02:00 +0200

    removed console statement

--- src/components/UiSrefActive.tsx
+++ src/components/UiSrefActive.tsx
@@ -4,33 +4,32 @@ import {find} from '../utils';
 
 export class UiSrefActive extends React.Component<any,any> {
     uiSref;
-    
+
     static propTypes = {
         class: React.PropTypes.string.isRequired,
         children: React.PropTypes.element.isRequired
     }
-    
+
     constructor (props) {
         super(props);
     }
-    
+
     componentDidMount () {
         const child = this.props.children;
         this.uiSref = find(child, component => {
             return typeof component.type === 'function' && component.type.name === 'UiSref';
         });
     }
-    
+
     isActive = () => {
         let currentState = Router.globals.current.name;
-        console.log(this.uiSref);
-        return this.uiSref && this.uiSref.props.to === currentState; 
+        return this.uiSref && this.uiSref.props.to === currentState;
     }
-    
+
     render () {
         let isActive = this.isActive();
         return (
-            !isActive 
+            !isActive
                 ? this.props.children
                 : React.cloneElement(this.props.children, { className: this.props.class })
         );
