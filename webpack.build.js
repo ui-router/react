9b91e9f0487e6d830e0b0a1118303673bdb23a77
Elbo <marco.botto@gmail.com>
2016-07-18 16:22:30 +0200

    set up webpack for build process in lib folder

--- webpack.build.js
+++ webpack.build.js
@@ -1,14 +1,28 @@
 var path = require("path");
+var webpack = require("webpack");
 
 var config = {
-	entry: ["./src/index.ts"],
+	entry: {
+		"ui-router-react": ["./src/index.ts"],
+		"ui-router-react.min": ["./src/index.ts"]
+	},
 	output: {
 		path: path.resolve(__dirname, "lib"),
-		filename: "ui-router-react.js"
+		filename: "[name].js",
+		libraryTarget: "umd",
+		library: "ui-router-react",
+		umdNamedDefine: true
 	},
 	resolve: {
 		extensions: ["", ".ts", ".tsx", ".js"]
 	},
+	devtool: 'source-map',
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			minimize: true,
+			include: /\.min\.js$/,
+		})
+	],
 	module: {
 		loaders: [
 			{
@@ -17,6 +31,14 @@ var config = {
 				exclude: /node_modules/
 			}
 		]
+	},
+	ts: {
+		compilerOptions: {
+			declaration: false
+		}
+	},
+	externals: {
+		"react": { root: 'react', amd: 'react', commonjs2: 'react', commonjs: 'react' }
 	}
 };
 
