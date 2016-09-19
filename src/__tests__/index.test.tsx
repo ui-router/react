9ad732ef1c47cbdf4d5feae3b378ec571bef6685
Marco Botto <marco.botto@gmail.com>
2016-09-19 13:20:58 +0200

    update(core): update core dependency to beta.2
    - update ui-router-core dependency
    - refactor $q and $injector as $QLike and $InjectorLike
    - remove url(url) and replace() in favor of setUrl(url, replace)
    
    closes #2

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,18 +1,24 @@
-declare var jest, describe, it, expect, beforeEach;
+declare var jest, describe, it, expect, beforeEach, afterEach;
 
 import * as React from "react";
 import { shallow, mount, render } from "enzyme";
 import * as sinon from "sinon";
 
 import UIRouterReact, {UIView, UISref, ReactStateDeclaration} from "../index";
-import {services} from 'ui-router-core';
+import {services, UrlMatcher} from 'ui-router-core';
 
 describe("UIRouterReact class", () => {
 
   let router;
-  let stub;
+  let sandbox;
+
   beforeEach(() => {
     router = new UIRouterReact();
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
   });
 
   it('creates a new instance correctly', () => { 
@@ -20,25 +26,67 @@ describe("UIRouterReact class", () => {
   });
 
   it('starts the router with start() method', () => {
-    let stub1 = sinon.stub(router.urlRouter, 'listen');
-    let stub2 = sinon.stub(router.urlRouter, 'sync');
+    let stub1 = sandbox.stub(router.urlRouter, 'listen');
+    let stub2 = sandbox.stub(router.urlRouter, 'sync');
     router.start();
     expect(stub1.calledOnce).toBe(true);
     expect(stub2.calledOnce).toBe(true);
   });
 
   it('defaults to hashLocation history implementation', () => {
-    stub = sinon.stub(window, 'addEventListener');
+    let stub = sandbox.stub(window, 'addEventListener');
     router = new UIRouterReact();
     services.location.onChange(() => {});
     expect(stub.firstCall.args[0]).toBe('hashchange');
   });
 
   it('sets pushState history implementation in html5 mode', () => {
+    let stub = sandbox.stub(window, 'addEventListener');
     router = new UIRouterReact();
     router.html5Mode(true);
     services.location.onChange(() => {});
-    expect(stub.secondCall.args[0]).toBe('popstate');
+    expect(stub.firstCall.args[0]).toBe('popstate');
   });
 
-})
+});
+
+describe('Location service', () => {
+
+  let router;
+  let stub;
+  let sandbox;
+  let makeMatcher;
+
+  beforeEach(() => {
+    router = new UIRouterReact();
+    sandbox = sinon.sandbox.create();
+    makeMatcher = (url, config?) => {
+      return new UrlMatcher(url, router.urlMatcherFactory.paramTypes, config)
+    }
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('in html5 mode', () => {
+
+    beforeEach(() => {
+      router.html5Mode(true);
+      router.start();
+    });
+
+    it('uses history.pushState when setting a url', () => {
+      let stub = sandbox.stub(history, 'pushState');
+      router.urlRouter.push(makeMatcher('/hello/:name'), { name: 'world' });
+      expect(stub.firstCall.args[2]).toBe('/hello/world');
+    });
+
+    it('uses history.replaceState when setting a url with replace', () => {
+      let stub = sandbox.stub(history, 'replaceState');
+      router.urlRouter.push(makeMatcher('/hello/:name'), { name: 'world' }, { replace: true });
+      expect(stub.firstCall.args[2]).toBe('/hello/world');
+    });
+  });
+
+});
--- src/justjs.ts
+++ src/justjs.ts
@@ -6,80 +6,76 @@
 import {services} from "ui-router-core";
 import {isDefined, isFunction, isArray, isObject, isInjectable} from "ui-router-core";
 import {extend, assertPredicate, forEach, applyPairs} from "ui-router-core";
+import {$QLike, $InjectorLike, IInjectable} from "ui-router-core";
 
 /** $q-like promise api */
-services.$q = (executor: (resolve, reject) => void) => new Promise(executor);
-services.$q.when = (val) => new Promise((resolve, reject) => resolve(val));
-services.$q.reject = (val) => new Promise((resolve, reject) => { reject(val); });
-services.$q.defer = function() {
-  let deferred: any = {};
-  deferred.promise = new Promise((resolve, reject) => {
-    deferred.resolve = resolve;
-    deferred.reject = reject;
-  });
-
-  return deferred;
-};
-
-services.$q.all = function (promises: { [key: string]: Promise<any> } | Promise<any>[]) {
-  if (isArray(promises)) {
-    return new Promise((resolve, reject) => {
-      let results = [];
-      promises.reduce((wait4, promise) => wait4.then(() => promise.then(val => results.push(val))), services.$q.when())
+services.$q = {
+  when: (val) => new Promise((resolve, reject) => resolve(val)),
+  reject: (val) => new Promise((resolve, reject) => { reject(val); }),
+  defer: () => {
+    let deferred: any = {};
+    deferred.promise = new Promise((resolve, reject) => {
+      deferred.resolve = resolve;
+      deferred.reject = reject;
+    });
+    return deferred;
+  },
+  all: (promises: { [key: string]: Promise<any> } | Promise<any>[]) => {
+    if (isArray(promises)) {
+      return new Promise((resolve, reject) => {
+        let results = [];
+        promises.reduce((wait4, promise) => wait4.then(() => promise.then(val => results.push(val))), services.$q.when())
           .then(() => { resolve(results); }, reject);
-    });
+      });
+    }
+
+    if (isObject(promises)) {
+      // console.log("$q.all({}) Input:", promises);
+
+      // Convert promises map to promises array.
+      // When each promise resolves, map it to a tuple { key: key, val: val }
+      let chain = Object.keys(promises)
+          .map(key => promises[key].then(val => ({key, val})));
+      // Then wait for all promises to resolve, and convert them back to an object
+      return services.$q.all(chain).then(values => {
+        let value = values.reduce((acc, tuple) => { acc[tuple.key] = tuple.val; return acc; }, {});
+
+        // console.log("$q.all({}) Output:", value);
+        return value;
+      });
+    }
   }
-
-  if (isObject(promises)) {
-    // console.log("$q.all({}) Input:", promises);
-
-    // Convert promises map to promises array.
-    // When each promise resolves, map it to a tuple { key: key, val: val }
-    let chain = Object.keys(promises)
-        .map(key => promises[key].then(val => ({key, val})));
-    // Then wait for all promises to resolve, and convert them back to an object
-    return services.$q.all(chain).then(values => {
-      let value = values.reduce((acc, tuple) => { acc[tuple.key] = tuple.val; return acc; }, {});
-
-      // console.log("$q.all({}) Output:", value);
-      return value;
-    });
-  }
-};
+} as $QLike;
 
 
-
-
-
-// angular1-like injector api
-
+/** angular1-like injector api */
 // globally available injectables
 let globals = { };
-services.$injector = { };
-
-services.$injector.get = name => globals[name];
-services.$injector.has = (name) => services.$injector.get(name) != null;
-services.$injector.invoke = function(fn, context?, locals?) {
-  let all = extend({}, globals, locals || {});
-  let params = services.$injector.annotate(fn);
-  let ensureExist = assertPredicate(key => all.hasOwnProperty(key), key => `DI can't find injectable: '${key}'`);
-  let args = params.filter(ensureExist).map(x => all[x]);
-  if (isFunction(fn)) return fn.apply(context, args);
-  return fn.slice(-1)[0].apply(context, args);
-};
-
 let STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
 let ARGUMENT_NAMES = /([^\s,]+)/g;
-// http://stackoverflow.com/questions/1007981
-services.$injector.annotate = function(fn) {
-  if (!isInjectable(fn)) throw new Error(`Not an injectable function: ${fn}`);
-  if (fn && fn.$inject) return fn.$inject;
-  if (isArray(fn)) return fn.slice(0, -1);
-  let fnStr = fn.toString().replace(STRIP_COMMENTS, '');
-  let result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-  return result || [];
-};
+services.$injector = {
+  get: name => globals[name],
+  has: (name) => services.$injector.get(name) != null,
+  invoke: (fn:IInjectable, context?, locals?) => {
+    let all = extend({}, globals, locals || {});
+    let params = services.$injector.annotate(fn);
+    let ensureExist = assertPredicate(key => all.hasOwnProperty(key), key => `DI can't find injectable: '${key}'`);
+    let args = params.filter(ensureExist).map(x => all[x]);
+    if (isFunction(fn)) return fn.apply(context, args);
+    else return (fn as any[]).slice(-1)[0].apply(context, args);
+  },
+  annotate: (fn:IInjectable): any[] => {
+    if (!isInjectable(fn)) throw new Error(`Not an injectable function: ${fn}`);
+    if (fn && (fn as any).$inject) return (fn as any).$inject;
+    if (isArray(fn)) return fn.slice(0, -1);
+    let fnStr = fn.toString().replace(STRIP_COMMENTS, '');
+    let result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+    return result || [];
+  }
+} as $InjectorLike;
 
+
+/** angular1-like location provider */
 let loc = <any> services.location;
 let hashPrefix: string = '';
 let baseHref: string = "";
@@ -102,15 +98,6 @@ locCfg.hashPrefix = (newprefix: string): string => {
   }
   return hashPrefix;
 };
-
-
-
-
-
-
-
-
-
 
 const beforeAfterSubstr = char => str => {
   if (!str) return ["", ""];
@@ -146,16 +133,14 @@ export let hashLocation = {
       splitHash(splitQuery(trimHashVal(location.hash))[0])[0],
   search: () =>
     getParams(splitQuery(splitHash(trimHashVal(location.hash))[1])[1]),
+  setUrl: (url: string, replace: boolean = true) => {
+    if (url) location.hash = url;
+  },
   url: (url) => {
-    if(isDefined(url)) {
-      location.hash = url;
-    }
     return loc.path();
   },
-  replace: () => console.log(new Error('$location.replace() not impl')),
   onChange: (cb) => window.addEventListener("hashchange", cb, false)
 };
-
 
 
 export let pushStateLocation = {
@@ -170,14 +155,16 @@ export let pushStateLocation = {
   },
   search: () =>
     getParams(splitQuery(location.search)[1]),
-  url: (url) => {
-    if (isDefined(url) && url !== loc.url()) {
-      history.pushState(null, null, locCfg.baseHref() + url);
+  setUrl: (url: string, replace: boolean = false) => {
+    if (isDefined(url)) {
+      if (replace) history.replaceState(null, null, locCfg.baseHref() + url);
+      else history.pushState(null, null, locCfg.baseHref() + url);
     }
+  },
+  url: (url) => {
     let hash = loc.hash();
     return loc.path() + (hash ? "#" + hash : "");
   },
-  replace: () => console.log(new Error('$location.replace() not impl')),
   onChange: (cb) => window.addEventListener("popstate", cb, false)
 };
 
