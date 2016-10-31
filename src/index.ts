8dcc50f02271932c3c30b480819aa79eed827ed4
Marco Botto <marco.botto@gmail.com>
2016-10-31 18:19:25 +0100

    chore(refactor): rename core module & reactViews

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,14 @@
-import {trace, UIRouter, PathNode, services} from 'ui-router-core';
-import './justjs';
-import {hashLocation, pushStateLocation} from './justjs';
-import {ReactViewConfig, reactViewsBuilder} from "./ui-router-react"
-import {ReactViewDeclaration, ReactStateDeclaration} from "./interface"
-import {UIView} from "./components/UIView";
-import {UISref} from "./components/UISref";
-import {UISrefActive} from "./components/UISrefActive";
+import './services';
+import UIRouterReact from "./core";
 
-// Set up view config factory
-let viewConfigFactory = (node: [PathNode], config: ReactViewDeclaration) =>
-  new ReactViewConfig(node, config);
+export {trace} from "ui-router-core";
+export {ReactStateDeclaration} from "./interface";
 
-export default class UIRouterReact extends UIRouter {
-  static instance;
-  constructor() {
-    super();
-    this.viewService.viewConfigFactory('react', viewConfigFactory);
-    this.stateRegistry.decorator("views", reactViewsBuilder);
-    UIRouterReact.instance = this;
-  }
-  start(): void {
-    this.stateRegistry.stateQueue.autoFlush(this.stateService);
-    this.urlRouter.listen();
-    this.urlRouter.sync();
-  }
-  html5Mode (mode: Boolean): void {
-    if (mode === true) {
-      let loc = <any> services.location;
-      let locCfg = <any> services.locationConfig;
-      locCfg.html5Mode = () => true;
-      Object.assign(loc, pushStateLocation);
-    }
-  }
-}
+export {browserHistory} from "./history/browserHistory";
+export {hashHistory} from "./history/hashHistory";
 
-export {
-    UIView,
-    UISref,
-    UISrefActive,
-    ReactStateDeclaration,
-    trace
-}
+export {UIView} from "./components/UIView";
+export {UISref} from "./components/UISref";
+export {UISrefActive} from "./components/UISrefActive";
+
+export default UIRouterReact;
