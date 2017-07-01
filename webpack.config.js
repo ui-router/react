8fc846c7dde1d09e5d2ec2bfbc1d64c28e238b7b
Chris Thielen <cthielen@netflix.com>
2017-07-01 08:52:11 -0700

    chore(webpack): tabs to spaces

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,40 @@ var path = require("path");
 var webpack = require("webpack");
 
 var config = {
-	entry: {
-		"ui-router-react": ["./src/index.ts"],
-		"ui-router-react.min": ["./src/index.ts"]
-	},
-	output: {
-		path: path.resolve(__dirname, "_bundles"),
-		filename: "[name].js",
-		libraryTarget: "umd",
-		library: "UIRouterReact",
-		umdNamedDefine: true
-	},
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"]
-	},
-        watch:  process.env.WATCH === 'true',
-	devtool: 'source-map',
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			minimize: true,
-			include: /\.min\.js$/,
-		})
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.tsx?$/,
-				loader: "awesome-typescript-loader",
-				exclude: /(node_modules|__tests__)/
-			}
-		]
-	},
-	externals: {
-		"react": { root: 'React', amd: 'react', commonjs2: 'react', commonjs: 'react' }
-	}
+  entry: {
+    "ui-router-react": ["./src/index.ts"],
+    "ui-router-react.min": ["./src/index.ts"]
+  },
+  output: {
+    path: path.resolve(__dirname, "_bundles"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "UIRouterReact",
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  watch:  process.env.WATCH === 'true',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      include: /\.min\.js$/,
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        exclude: /(node_modules|__tests__)/
+      }
+    ]
+  },
+  externals: {
+    "react": { root: 'React', amd: 'react', commonjs2: 'react', commonjs: 'react' }
+  }
 };
 
 module.exports = config;
