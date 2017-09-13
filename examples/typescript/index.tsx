091403af4ef63debc3769161ffe8e3c4b02504c8
Marco Botto <marco.botto@gmail.com>
2017-09-13 20:44:28 +0100

    chore(examples): update base example to react@16.0.0-rc2
    chore(*): add `start` script for running base typescript example as dev
    environment

--- examples/typescript/index.tsx
+++ examples/typescript/index.tsx
@@ -1,47 +1,74 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { UIRouter, UIRouterReact, UIView, UISrefActive, UISref, ReactStateDeclaration, ReactViewDeclaration, trace, pushStateLocationPlugin } from '@uirouter/react';
+import {
+  UIRouter,
+  UIRouterReact,
+  UIView,
+  UISrefActive,
+  UISref,
+  ReactStateDeclaration,
+  ReactViewDeclaration,
+  trace,
+  pushStateLocationPlugin,
+} from '@uirouter/react';
 
 import { Child } from './components/Child';
 import { Header } from './components/Header';
 import { Home } from './components/Home';
 import { Nest } from './components/Nest';
 
-let home:ReactStateDeclaration = { name: 'home', component: Home, url: '/home?foo' };
-let child:ReactStateDeclaration = { name: 'home.child', component: Child, url: '/child' };
-let nest:ReactStateDeclaration = {
+let home: ReactStateDeclaration = {
+  name: 'home',
+  component: Home,
+  url: '/home?foo',
+};
+let child: ReactStateDeclaration = {
+  name: 'home.child',
+  component: Child,
+  url: '/child',
+};
+let nest: ReactStateDeclaration = {
   name: 'home.child.nest',
   views: {
     $default: Nest as ReactViewDeclaration,
-    "header@home": Header as ReactViewDeclaration
+    'header@home': Header as ReactViewDeclaration,
   },
   url: '/nest',
-  resolve: [{
-    token: 'foo',
-    resolveFn: ($transition$) => {
-      return new Promise<string>((resolve, reject) => {
-        setTimeout(() => {
-          resolve('bar');
-        }, 1000);
-      });
-    }
-  }]
+  resolve: [
+    {
+      token: 'foo',
+      resolveFn: $transition$ => {
+        return new Promise<string>((resolve, reject) => {
+          setTimeout(() => {
+            resolve('bar');
+          }, 1000);
+        });
+      },
+    },
+  ],
 };
 
-
 const routerConfig = (router: UIRouterReact) => {
-  router.urlRouter.otherwise("/home");
+  router.urlRouter.otherwise('/home');
   trace.enable(1);
-}
+};
 
-let el = document.getElementById("react-app");
+let el = document.getElementById('react-app');
 let app = (
-  <UIRouter plugins={[pushStateLocationPlugin]} states={[home, child, nest]} config={routerConfig}>
+  <UIRouter
+    plugins={[pushStateLocationPlugin]}
+    states={[home, child, nest]}
+    config={routerConfig}
+  >
     <div>
       <UISrefActive class="active">
-        <UISref to="home"><a>Home</a></UISref>
+        <UISref to="home">
+          <a>Home</a>
+        </UISref>
       </UISrefActive>
-      <UIView render={(Comp, props) => <Comp {...props} foo="bar" />}><p>Content will load here</p></UIView>
+      <UIView render={(Comp, props) => <Comp {...props} foo="bar" />}>
+        <p>Content will load here</p>
+      </UIView>
     </div>
   </UIRouter>
 );
--- examples/typescript/webpack.config.js
+++ examples/typescript/webpack.config.js
@@ -1,24 +1,28 @@
-var path = require("path");
+var path = require('path');
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+
+var configFilePath = path.resolve(__dirname, 'tsconfig.json');
 
 var config = {
-  entry: ["./index.tsx"],
+  entry: [path.resolve(__dirname, './index.tsx')],
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsConfigPathsPlugin({ configFileName: configFilePath })],
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     loaders: [
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader",
-        exclude: /(node_modules|__tests__)/
-      }
-    ]
-  }
+        loader: 'awesome-typescript-loader',
+        exclude: /(node_modules|__tests__)/,
+      },
+    ],
+  },
 };
 
 module.exports = config;
