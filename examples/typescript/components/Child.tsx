4dd0c37d72d4ef8510974e4c309a92025c90c204
elboman <marco.botto@gmail.com>
2018-05-09 01:12:24 +0100

    chore(example): add remount example in child state

--- examples/typescript/components/Child.tsx
+++ examples/typescript/components/Child.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
-import { UIView } from '@uirouter/react';
+import { UIView, UIViewInjectedProps } from '@uirouter/react';
 
-export class Child extends React.Component<any, any> {
+export class Child extends React.Component<UIViewInjectedProps, any> {
   uiCanExit = () => {
     return Promise.resolve();
+  };
+  componentDidMount() {
+    console.log('mounted');
+  }
+  handleClick = () => {
+    this.props.transition.router.stateService.reload();
   };
   render() {
     return (
       <div>
         <h2>Child</h2>
         <UIView />
+        <button onClick={this.handleClick}>remount</button>
       </div>
     );
   }
