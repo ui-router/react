3a136eb6370adc792ecf871dc6dcee215cd649cd
Chris Thielen <cthielen@netflix.com>
2020-01-02 19:13:17 -0800

    feat(hooks): Add useDeepObjectDiff hook to detect changes to params or config objects in other hooks

---
+++ src/components/hooks/useDeepObjectDiff.ts
@@ -0,0 +1,14 @@
+import { equals } from '@uirouter/core';
+import { useRef } from 'react';
+
+/** Internal hook to support deep diffing in hook dependencies */
+export function useDeepObjectDiff(obj: object): number {
+  const ref = useRef(obj);
+  const version = useRef(1);
+  const deepEqual = obj === ref.current || equals(obj, ref.current);
+  ref.current = obj;
+  if (!deepEqual) {
+    version.current++;
+  }
+  return version.current;
+}
