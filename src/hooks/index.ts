d31779043370628cb5ac94aa80a724e100d186a5
Chris Thielen <cthielen@netflix.com>
2020-01-05 17:47:00 -0800

    fix(useSrefActive): Export useSrefActive and useSrefActiveExact

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,7 @@ export * from './useCurrentStateAndParams';
 export * from './useOnStateChanged';
 export * from './useRouter';
 export * from './useSref';
+export * from './useSrefActive';
+export * from './useSrefActiveExact';
 export * from './useStableCallback';
 export * from './useTransitionHook';
