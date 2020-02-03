a36fd59f1cea82db47ffe272bfe71ed44c0a9987
Chris Thielen <cthielen@netflix.com>
2020-02-03 10:13:23 -0800

    refactor(UIView): Refactor UIView as a functional component

--- src/components/UIView.tsx
+++ src/components/UIView.tsx
@@ -1,31 +1,38 @@
 /** @packageDocumentation @reactapi @module components */
-import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import * as React from 'react';
 import {
-  ClassType,
-  ClassicComponentClass,
-  Component,
-  ComponentClass,
-  createContext,
-  SFC,
+  ComponentType,
   ReactNode,
-  StatelessComponent,
   ValidationMap,
-  Validator,
   cloneElement,
+  createContext,
   createElement,
   isValidElement,
+  useEffect,
+  useMemo,
+  useState,
 } from 'react';
-
-import { ActiveUIView, ViewContext, Transition, ResolveContext, StateParams, applyPairs } from '@uirouter/core';
-
-import { UIRouterContext } from './UIRouter';
-import { UIRouterReact } from '../core';
+import {
+  ActiveUIView,
+  ResolveContext,
+  StateParams,
+  Transition,
+  TypedMap,
+  UIInjector,
+  ViewConfig,
+  ViewContext,
+  applyPairs,
+} from '@uirouter/core';
+import { useParentView } from '../hooks/useParentView';
+import { useRouter } from '../hooks/useRouter';
+import { useTransitionHook } from '../hooks/useTransitionHook';
 import { ReactViewConfig } from '../reactViews';
-import { UIRouterInstanceUndefinedError } from '../hooks/useRouter';
 
 /** @internalapi */
 let id = 0;
+/** @hidden */
+let keyCounter = 0;
 
 /** @internalapi */
 export interface UIViewAddress {
@@ -64,10 +71,7 @@ export interface UIViewResolves {
  * If the `render` function prop is provided, the `UIView` will use it instead of rendering the component by itself.
  * @internalapi
  */
-export type RenderPropCallback = (
-  Component: StatelessComponent<any> | ComponentClass<any> | ClassicComponentClass<any>,
-  Props: any
-) => JSX.Element | null;
+export type RenderPropCallback = (Component: ComponentType<any>, Props: any) => JSX.Element | null;
 
 export interface UIViewInjectedProps {
   transition?: Transition;
@@ -79,20 +83,10 @@ export interface UIViewInjectedProps {
 /** Component Props for `UIView` */
 export interface UIViewProps {
   children?: ReactNode;
-  router?: UIRouterReact;
-  parentUIView?: UIViewAddress;
   name?: string;
   className?: string;
   style?: Object;
   render?: RenderPropCallback;
-}
-
-/** Component State for `UIView` */
-export interface UIViewState {
-  id?: number;
-  loaded?: boolean;
-  component?: string | SFC<any> | ClassType<any, any, any> | ComponentClass<any>;
-  props?: any;
 }
 
 export const TransitionPropCollisionError =
@@ -102,190 +96,116 @@ export const TransitionPropCollisionError =
 /** @internalapi */
 export const UIViewContext = createContext<UIViewAddress>(undefined);
 
-class View extends Component<UIViewProps, UIViewState> {
-  // This object contains all the metadata for this UIView
-  uiViewData: ActiveUIView;
-
-  // This object contains only the state context which created this <UIView/> component
-  // and the UIView's fully qualified name. This object is made available to children via `context`
-  uiViewAddress: UIViewAddress;
-
-  // Deregisters the UIView when it is unmounted
-  deregister: Function;
-
-  // Bind the rendered component instance in order to call its uiCanExit hook
-  componentInstance: any;
-  // Removes th Hook when the UIView is unmounted
-  removeHook: Function;
-
-  state: UIViewState = {
-    loaded: false,
-    component: 'div',
-    props: {},
-  };
-
-  static propTypes: ValidationMap<UIViewProps> = {
-    router: PropTypes.object.isRequired as Validator<UIRouterReact>,
-    parentUIView: PropTypes.object as Validator<UIViewAddress>,
-    name: PropTypes.string,
-    className: PropTypes.string,
-    style: PropTypes.object,
-    render: PropTypes.func,
-  };
-
-  render() {
-    let { children, render } = this.props;
-    let { component, props, loaded } = this.state;
-    // register reference of child component
-    // register new hook right after component has been rendered
-    let stateName: string = this.uiViewAddress && this.uiViewAddress.context && this.uiViewAddress.context.name;
-
-    // only class components can implement the
-    // uiCanExit hook and ref doesn't work on
-    // stateless function components
-    if (
-      typeof component !== 'string' &&
-      (!!component.render || (component.prototype && !!component.prototype.render))
-    ) {
-      props.ref = c => {
-        this.componentInstance = c;
-        this.registerUiCanExitHook(stateName);
-      };
-    }
-
-    // attach any style or className to the rendered component
-    // specified on the UIView itself
-    const { className, style } = this.props;
-    const styleProps = { className, style };
-    const childProps = { ...props, ...styleProps };
-
-    let child =
-      !loaded && isValidElement(children) ? cloneElement(children, childProps) : createElement(component, childProps);
-
-    // if a render function is passed use that,
-    // otherwise render the component normally
-    const ChildOrRenderFunction = typeof render !== 'undefined' && loaded ? render(component, childProps) : child;
-    return <UIViewContext.Provider value={this.uiViewAddress}>{ChildOrRenderFunction}</UIViewContext.Provider>;
-  }
-
-  componentDidMount() {
-    const router = this.props.router;
-    if (typeof router === 'undefined') {
-      throw new Error(UIRouterInstanceUndefinedError);
-    }
-
-    // Check the context for the parent UIView's fqn and State
-    let parent: UIViewAddress = this.props.parentUIView;
-    // Not found in context, this is a root UIView
-    parent = parent || { fqn: '', context: router.stateRegistry.root() };
-
-    let name = this.props.name || '$default';
-
-    this.uiViewData = {
-      $type: 'react',
-      id: ++id,
-      name: name,
-      fqn: parent.fqn ? parent.fqn + '.' + name : name,
-      creationContext: parent.context,
-      configUpdated: this.viewConfigUpdated.bind(this),
-      config: undefined,
-    } as ActiveUIView;
-
-    this.uiViewAddress = { fqn: this.uiViewData.fqn, context: undefined };
-
-    this.deregister = router.viewService.registerUIView(this.uiViewData);
-
-    this.setState({ id: this.uiViewData.id });
-  }
-
-  componentWillUnmount() {
-    if (this.deregister) {
-      this.deregister();
-    }
-  }
-
-  /**
-   * View config updated callback
-   *
-   * This is called by UI-Router during ViewService.sync().
-   * The `newConfig` parameter will contain view configuration (component, etc) when a
-   * state is activated and one of its views targets this `UIView`.
-   */
-  viewConfigUpdated(newConfig: ReactViewConfig) {
-    if (newConfig === this.uiViewData.config) {
-      return;
-    }
-
-    let trans: Transition;
-    let resolves = {};
-
-    if (newConfig) {
-      let viewContext: ViewContext = newConfig.viewDecl && newConfig.viewDecl.$context;
-      this.uiViewAddress = {
-        fqn: this.uiViewAddress.fqn,
-        context: viewContext,
-      };
-
-      let resolveContext = new ResolveContext(newConfig.path);
-      let injector = resolveContext.injector();
-
-      let stringTokens: string[] = resolveContext.getTokens().filter(x => typeof x === 'string');
+/** @hidden */
+function useResolvesWithStringTokens(resolveContext: ResolveContext, injector: UIInjector) {
+  return useMemo(() => {
+    if (resolveContext && injector) {
+      const stringTokens: string[] = resolveContext.getTokens().filter(x => typeof x === 'string');
       if (stringTokens.indexOf('transition') !== -1) {
         throw new Error(TransitionPropCollisionError);
       }
-
-      trans = injector.get(Transition);
-      resolves = stringTokens.map(token => [token, injector.get(token)]).reduce(applyPairs, {});
+      return stringTokens.map(token => [token, injector.get(token)]).reduce(applyPairs, {});
+    } else {
+      return {};
     }
-
-    this.uiViewData.config = newConfig;
-    const key = Date.now();
-    let props = { ...resolves, transition: trans, key };
-
-    let newComponent = newConfig && newConfig.viewDecl && newConfig.viewDecl.component;
-    this.setState({
-      component: newComponent || 'div',
-      props: newComponent ? props : {},
-      loaded: !!newComponent,
-    });
-  }
-
-  registerUiCanExitHook(stateName: string) {
-    typeof this.removeHook === 'function' && this.removeHook();
-    let criteria = { exiting: stateName };
-    let callback =
-      this.componentInstance &&
-      typeof this.componentInstance.uiCanExit === 'function' &&
-      this.componentInstance.uiCanExit;
-    if (stateName && callback) {
-      let transitions = this.props.router.transitionService;
-      this.removeHook = transitions.onBefore(criteria, callback, {});
-    }
-  }
+  }, [resolveContext, injector]);
 }
 
-View.propTypes = {
-  router: PropTypes.object.isRequired as Validator<UIRouterReact>,
-  parentUIView: PropTypes.object as Validator<UIViewAddress>,
+/* @hidden These are the props are passed to the routed component. */
+function useChildProps(
+  component: React.FunctionComponent<any> | React.ComponentClass<any> | React.ClassicComponentClass<any>,
+  resolves: TypedMap<any> | {},
+  className: string,
+  style: Object,
+  transition: any,
+  key: string,
+  setComponentInstance: (instance: any) => void
+): UIViewInjectedProps {
+  return useMemo(() => {
+    const componentProps: UIViewInjectedProps & { key: string } = {
+      // spread each string resolve as a separate prop
+      ...resolves,
+      // if a className prop was passed to the UIView, forward it
+      className,
+      // if a style prop was passed to the UIView, forward it
+      style,
+      // the transition
+      transition,
+      // this key updates whenever the state is reloaded, causing the component to remount
+      key,
+    };
+
+    const maybeComponent = component as any;
+    if (maybeComponent?.prototype?.render || !!maybeComponent?.render) {
+      // for class components, add a ref to grab the component instance
+      return { ...componentProps, ref: setComponentInstance };
+    } else {
+      setComponentInstance(undefined);
+      return componentProps;
+    }
+  }, [component, resolves, className, style, transition, key]);
+}
+
+export function UIView(props: UIViewProps) {
+  const { children, render, className, style } = props;
+
+  const router = useRouter();
+  const parent = useParentView();
+
+  // If a class component is being rendered, this is the component instance
+  const [componentInstance, setComponentInstance] = useState();
+  const [viewConfig, setViewConfig] = useState<ReactViewConfig>();
+  const component = useMemo(() => viewConfig?.viewDecl?.component, [viewConfig]);
+
+  const name = props.name || '$default';
+
+  // This object contains all the metadata for this UIView
+  const uiViewData: ActiveUIView = useMemo(() => {
+    return {
+      $type: 'react',
+      id: ++id,
+      name,
+      fqn: parent.fqn ? parent.fqn + '.' + name : name,
+      creationContext: parent.context,
+      configUpdated: config => setViewConfig(config as ReactViewConfig),
+      config: viewConfig as ViewConfig,
+    };
+  }, [name, parent, viewConfig]);
+
+  const viewContext: ViewContext = uiViewData?.config?.viewDecl?.$context;
+  const uiViewAddress: UIViewAddress = { fqn: uiViewData.fqn, context: viewContext };
+  const stateName: string = uiViewAddress?.context?.name;
+  const resolveContext = useMemo(() => (viewConfig ? new ResolveContext(viewConfig.path) : undefined), [viewConfig]);
+  const injector = useMemo(() => resolveContext?.injector(), [resolveContext]);
+  const transition = useMemo(() => injector?.get(Transition), [injector]);
+  const resolves = useResolvesWithStringTokens(resolveContext, injector);
+  const key = useMemo(() => (++keyCounter).toString(), [viewConfig]);
+  const childProps = useChildProps(component, resolves, className, style, transition, key, setComponentInstance);
+
+  // Register/deregister any time the uiViewData changes
+  useEffect(() => router.viewService.registerUIView(uiViewData), [uiViewData]);
+
+  // Handle component class with a 'uiCanExit()' method
+  const canExitCallback = componentInstance?.uiCanExit;
+  const hookMatchCriteria = canExitCallback ? { exiting: stateName } : undefined;
+  useTransitionHook('onBefore', hookMatchCriteria, canExitCallback);
+
+  const childElement =
+    !component && isValidElement(children)
+      ? cloneElement(children, childProps)
+      : createElement(component || 'div', childProps);
+
+  // if a render function is passed, use that. otherwise render the component normally
+  const ChildOrRenderFunction =
+    typeof render !== 'undefined' && component ? render(component, childProps) : childElement;
+  return <UIViewContext.Provider value={uiViewAddress}>{ChildOrRenderFunction}</UIViewContext.Provider>;
+}
+
+UIView.displayName = 'UIView';
+UIView.__internalViewComponent = UIView;
+UIView.propTypes = {
   name: PropTypes.string,
   className: PropTypes.string,
   style: PropTypes.object,
   render: PropTypes.func,
 } as ValidationMap<UIViewProps>;
-
-export class UIView extends Component<UIViewProps, any> {
-  static displayName = 'UIView';
-  static __internalViewComponent: ComponentClass<UIViewProps> = View;
-
-  render() {
-    return (
-      <UIRouterContext.Consumer>
-        {router => (
-          <UIViewContext.Consumer>
-            {parentUIView => <View {...this.props} router={router} parentUIView={parentUIView} />}
-          </UIViewContext.Consumer>
-        )}
-      </UIRouterContext.Consumer>
-    );
-  }
-}
