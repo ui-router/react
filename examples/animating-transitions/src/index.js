3520596f6bb05f4405ed14953fbdf525bb5e1a6c
Chris Thielen <cthielen@netflix.com>
2020-03-29 12:46:41 -0700

    fix(examples): update animating-transitions example

--- examples/animating-transitions/src/index.js
+++ examples/animating-transitions/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { CSSTransitionGroup } from 'react-transition-group';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { UIRouter, UIView, hashLocationPlugin } from '@uirouter/react';
 
 import './index.css';
@@ -21,15 +21,19 @@ const states = [
 ];
 const config = router => router.urlService.rules.initial({ state: 'home' });
 
+const FadeTransition = props => <CSSTransition {...props} classNames="example" timeout={{ enter: 500, exit: 300 }} />;
+
 const App = () => (
   <UIRouter plugins={[hashLocationPlugin]} states={states} config={config}>
     <div>
       <Menu />
       <UIView
         render={(RoutedComponent, props) => (
-          <CSSTransitionGroup transitionName="example" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
-            <RoutedComponent {...props} key={props.transition} />
-          </CSSTransitionGroup>
+          <TransitionGroup>
+            <FadeTransition key={props.transition}>
+              <RoutedComponent {...props} />
+            </FadeTransition>
+          </TransitionGroup>
         )}
       />
     </div>
