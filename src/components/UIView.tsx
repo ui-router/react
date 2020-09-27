34dbea38dab9e7f910c2dcfe02eba4907dd36bd6
Chris Thielen <github@sandgnat.com>
2020-09-27 11:52:41 -0700

    refactor(canExit): Migrate class level canExit hook to its own file

--- src/components/UIView.tsx
+++ src/components/UIView.tsx
@@ -29,6 +29,7 @@ import { useStableCallback } from '../hooks';
 import { useMountStatusRef } from '../hooks/useMountStatusRef';
 import { useRouter } from '../hooks/useRouter';
 import { ReactViewConfig } from '../reactViews';
+import { useUiCanExitClassComponentHook } from './hooks/useUiCanExitClassComponent.hook';
 
 /** @internal */
 let viewIdCounter = 0;
@@ -171,43 +172,6 @@ function useContentComponentProps(
 interface ReactViewState {
   uiViewPortalRenderCommand: UIViewPortalRenderCommand;
   ContentComponent: React.ComponentType;
-}
-
-/**
- * If a class component is being rendered, wire up its uiCanExit method
- * Return a { ref: Ref<ClassComponentInstance> } if passed a component class
- * Return an empty object {} if passed anything else
- * The returned object should be spread as props onto the child component
- * @internal
- */
-function useUiCanExitClassComponentHook(router: UIRouter, stateName: string, maybeComponentClass: any) {
-  // Use refs and run the callback outside of any render pass
-  const componentInstanceRef = useRef<any>();
-  const deregisterRef = useRef<Function>(() => undefined);
-
-  function callbackRef(componentInstance) {
-    // Use refs
-    const previous = componentInstanceRef.current;
-    const deregisterPreviousTransitionHook = deregisterRef.current;
-
-    if (previous !== componentInstance) {
-      componentInstanceRef.current = componentInstance;
-      deregisterPreviousTransitionHook();
-
-      const uiCanExit = componentInstance?.uiCanExit;
-      if (uiCanExit) {
-        const boundCallback = uiCanExit.bind(componentInstance);
-        deregisterRef.current = router.transitionService.onBefore({ exiting: stateName }, boundCallback);
-      } else {
-        deregisterRef.current = () => undefined;
-      }
-    }
-  }
-
-  return useMemo(() => {
-    const isComponentClass = maybeComponentClass?.prototype?.render || maybeComponentClass?.render;
-    return isComponentClass ? { ref: callbackRef } : undefined;
-  }, [maybeComponentClass]);
 }
 
 const View = forwardRef(function View(props: UIViewProps, forwardedRef) {
